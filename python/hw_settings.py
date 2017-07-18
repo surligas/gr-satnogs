2f5393a8a23733cf9941274139678d418601aea2
Manolis Surligas <surligas@gmail.com>
2017-07-18 16:35:51 +0300

    Reduce RTL hardware buffer size
    For memory limited devices like the Rpi, the default memory allocation
    size of the RTL driver can be too great. This prevented some flowgraphs
    from statrting.  Using the proper device arguments on the Osmocom source
    block, we reduce thea allocated memory in a well suited value.

--- python/hw_settings.py
+++ python/hw_settings.py
@@ -47,4 +47,4 @@ hw_rx_settings = {'usrpb200' : {'rf_gain' : 50.0, 'if_gain' : 0.0,
                               'antenna' : '', 'dev_arg': 'hackrf'},
                   'rtlsdr' : {'rf_gain' : 32.8, 'if_gain' : 0.0,
                               'bb_gain' : 0.0, 'samp_rate' : 1e6,
-                              'antenna' : '', 'dev_arg' : 'rtl'} }
+                              'antenna' : '', 'dev_arg' : 'rtl,buffers=32,buflen=16384'} }
